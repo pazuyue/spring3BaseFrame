cedd8d2b38d1966e549f7c0cb3c457afb8e4d320
yueguang@qingmutec.com <yueguang@qingmutec.com>
2023-05-19 11:42:11 +0800

    初始化 - 日志组件

--- src/main/java/com/example/springdemo/Controller/UserController.java
+++ src/main/java/com/example/springdemo/Controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.springdemo.Controller;
 
 import com.example.springdemo.Interface.ControllerWebLog;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping("/getOne")
     @ControllerWebLog(name = "查询", intoDb = true)
     public String getOne(Long id, String name) {
         return "1234";
+    }
+
+    @GetMapping("/testLevel")
+    @ControllerWebLog(name = "查询", intoDb = true)
+    public void testLevel(){
+        logger.trace(" --- trace --- ");
+        logger.debug(" --- debug --- ");
+        logger.info(" --- info --- ");
+        logger.warn(" --- warn --- ");
+        logger.error(" --- error --- ");
     }
 }
