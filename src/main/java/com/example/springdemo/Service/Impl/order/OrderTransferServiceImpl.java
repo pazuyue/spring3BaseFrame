25b7460608657022b7ce36457ce578de18b7ae00
yueguang <yueguang@qingmutec.com>
2023-05-30 22:57:20 +0800

    初始化 -  多数据源

--- src/main/java/com/example/springdemo/Service/Impl/order/OrderTransferServiceImpl.java
+++ src/main/java/com/example/springdemo/Service/Impl/order/OrderTransferServiceImpl.java
@@ -1,8 +1,12 @@
 package com.example.springdemo.Service.Impl.order;
 
 import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.TypeReference;
 import com.example.springdemo.Entity.TChannel.TChannel;
+import com.example.springdemo.Entity.TChannelOrderLog.TChannelOrderLog;
 import com.example.springdemo.Service.Impl.TChannel.TChannelServiceImpl;
+import com.example.springdemo.Service.TChannelOrderLog.TChannelOrderLogService;
 import com.example.springdemo.Service.order.OrderTransferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,10 +20,18 @@ public class OrderTransferServiceImpl implements OrderTransferService {
     @Autowired
     private TChannelServiceImpl tChannelService;
 
+    @Autowired
+    private TChannelOrderLogService tChannelOrderLogService;
+
     @Override
     public boolean autoOrderMigration(int channelId, int orderLogId) {
         TChannel tChannel= tChannelService.getTChannel(channelId);
-        //Map map = JSON.parseObject(data, Map.class);
+        TChannelOrderLog tChannelOrderLog = tChannelOrderLogService.getById(orderLogId);
+        String json = tChannelOrderLog.getContent();
+        Map<String,Object> map = JSONObject.parseObject(json,new TypeReference<HashMap<String,Object>>(){});
+        System.out.println("map:"+map);
+        //Map order= map["trade_fullinfo_get_response"]['trade'];
+
         return false;
     }
 
