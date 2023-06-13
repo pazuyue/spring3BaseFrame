a7141a923f770d23e7134815bf04007f0cd259b2
yueguang <yueguang@qingmutec.com>
2023-06-13 10:47:50 +0800

    订单模块 - 手动分库

--- src/main/java/com/example/springdemo/Service/Impl/User/UserServiceImpl.java
+++ src/main/java/com/example/springdemo/Service/Impl/User/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package com.example.springdemo.Service.Impl.User;
 
+import com.baomidou.dynamic.datasource.annotation.DS;
 import com.example.springdemo.Entity.User.User;
 import com.example.springdemo.Mapper.User.UserMapper;
 import com.example.springdemo.Service.User.UserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * <p>
@@ -24,5 +27,11 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
     {
         this.baseMapper.insert(user);
         return 1/0;
+    }
+
+    @DS("#dataBaseName")//使用spel从参数获取
+    public List<User> list(String dataBaseName)
+    {
+       return this.list();
     }
 }
---
+++ src/test/java/com/example/springdemo/MybatisTest.java
@@ -0,0 +1,25 @@
+package com.example.springdemo;
+
+import com.baomidou.dynamic.datasource.toolkit.DynamicDataSourceContextHolder;
+import com.example.springdemo.Service.Impl.User.UserServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest()
+public class MybatisTest {
+    @Autowired
+    private UserServiceImpl userService;
+
+
+    @Test
+    public void testSelectUser()
+    {
+        DynamicDataSourceContextHolder.push("master");//手动切换
+        System.out.println(userService.list());
+
+    }
+}
