77a0e10a57e834ca9e06a78d99f1e9eefe931ee7
yueguang <yueguang@qingmutec.com>
2024-01-18 14:01:27 +0800

    死信队列配置

--- src/main/java/com/example/springdemo/Config/RabbitMq/MQProperties.java
+++ src/main/java/com/example/springdemo/Config/RabbitMq/MQProperties.java
@@ -11,7 +11,13 @@ import java.util.Map;
 @Configuration
 @ConfigurationProperties(prefix = "mq")
 public class MQProperties {
-    private String defaultExchange;
+    private String defaultExchange; //一般交换机
     private Map<String,String> queueAndRouteKey;
+    private String deadExchange; //死信交换机
+    private String deadQueue; //死信队列
+    private String deadRoutingKey;
+    private String deadRoutingKeyLabel; //死信路由键的标志
+    private String deadExchangeLabel; //死信交换器的标志
+
 }
 
