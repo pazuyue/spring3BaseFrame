26b573850aadc5b93bc8c32f1fc4bd3497f5cba2
yueguang <yueguang@qingmutec.com>
2024-03-13 19:06:23 +0800

    队列管理

--- src/main/java/com/example/springdemo/Service/Impl/Rabbit/RabbitMqConsumerService.java
+++ src/main/java/com/example/springdemo/Service/Impl/Rabbit/RabbitMqConsumerService.java
@@ -4,6 +4,7 @@ import cn.hutool.extra.spring.SpringUtil;
 import com.example.springdemo.Service.Queues.QueueService;
 import com.example.springdemo.Utils.RabbitMQ.RabbitMQUtils;
 import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.MessageProperties;
@@ -12,21 +13,20 @@ import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class RabbitMqConsumerService {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMqConsumerService.class);
 
     //@RabbitListener(queues = "${mq.queues}")
     @RabbitListener(queues = "#{'${mq.queues}'.split(',')}")
     public void receive(String payload, Channel channel, MessageProperties properties, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws InterruptedException {
-        LOGGER.info("消费properties：{}", properties);
-        LOGGER.info("消费channel：{}", channel);
+        log.info("消费properties：{}", properties);
+        log.info("消费channel：{}", channel);
         String queue = properties.getConsumerQueue();
         String queueBean = queue+"ServiceImpl";
         QueueService queueService = SpringUtil.getBean(queueBean);
         queueService.handle(payload);
         Thread.sleep(1000); // 休眠一秒，好看效果
-        RabbitMQUtils.askMessage(channel, tag, LOGGER);
+        RabbitMQUtils.askMessage(channel, tag, log);
     }
 }
